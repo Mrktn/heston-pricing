cb0079114c98e863e582933a32fab0cb7f9bc53a
Antoine Balestrat <antoine.balestrat@gmail.com>
2019-02-18 19:41:28 +0100

    Removed cpp file for consistency reasons

--- src/Brownian1D.hpp
+++ src/Brownian1D.hpp
@@ -11,13 +11,17 @@ class Brownian1D : public Trajectory1D {
 
 public:
     // Constructor for uniform time steps
-    Brownian1D(double h, unsigned n);
+    Brownian1D(double h, unsigned n) : Trajectory1D(h, n), G(0, 1) { };
 
     // Constructor for non uniform time steps
-    Brownian1D(const std::function<double(unsigned)> & gamma, unsigned n);
+    Brownian1D(const std::function<double(unsigned)> & gamma, unsigned n) : Trajectory1D(gamma, n), G(0, 1) { };
 
     // Returns the values of a newly generated path
-    std::vector<double> & operator()(std::mt19937_64 & gen);
+    std::vector<double> & operator()(std::mt19937_64 & gen) {
+        for (unsigned i = 1; i < values.size(); ++i)
+            values[i] = values[i - 1] + std::sqrt(times[i] - times[i - 1]) * G(gen);
+        return values;
+    };
 
 protected:
     std::normal_distribution<> G;
