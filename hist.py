2df78f9d4d030cd69b96df74557bbb806c351636
Antoine Balestrat <antoine.balestrat@gmail.com>
2019-03-01 19:43:58 +0100

    Heavily reduced variance thanks to call-put parity as a control variate. Phew, what an improvement it turns out to be !

--- hist.py
+++ hist.py
@@ -10,8 +10,11 @@ if len(sys.argv) < 2:
 with open(sys.argv[1]) as f:
     l = []
     for x in f:
-        l.append(float(x))
+        try:
+            l.append(float(x))
+        except ValueError:
+            pass
 
     plt.hist(l)
-    plt.title("Average = " + str(round(np.mean(l), 3)) + ", std = " + str(round(np.std(l), 3)))
+    plt.title("Average = " + str(round(np.mean(l), 5)) + ", std = " + str(round(np.std(l), 5)))
     plt.show()
